284515a9bd53a40762efa16dd685321a33e706ee
William Grant <willmgrant@gmail.com>
2021-08-06 14:05:49 +0200

    added favicon and setup initial seo metadata

---
+++ constants/metadata.ts
@@ -0,0 +1,25 @@
+const METADATA = {
+  HOST_URL: 'https://getsession.org',
+  SITE_NAME: 'Session',
+  TITLE: 'Session | Send Messages, Not Metadata. | Private Messenger',
+  DESCRIPTION:
+    'Session is a private messenger that aims to remove any chance of metadata collection by routing all messages through an onion routing network.',
+  IMAGE: {
+    URL: '/assets/images/logo-black.png',
+    WIDTH: 804,
+    HEIGHT: 665,
+    ALT: 'Session Logo Black Background',
+  },
+  LOCALE: 'en_US',
+  FAVICON: {
+    MEDIUM: '/favicon-32x32.png',
+    SMALL: '/favicon-16x16.png',
+    APPLE_TOUCH_ICON: '/apple-touch-icon.png',
+  },
+  MANIFEST: '/site.webmanifest',
+  MASK_ICON: { PATH: '/safari-pinned-tab.svg', COLOR: '#00f782' },
+  MSAPPLICATION_TILECOLOR: '#343132',
+  THEME_COLOR: '#ffffff',
+};
+
+export default METADATA;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,117 @@
 import { AppProps } from 'next/app';
-import '../styles/globals.css';
+import '@/styles/globals.css';
 import lockPageTitle from '@/utils/lockPageTitle';
+import { DefaultSeo } from 'next-seo';
+import METADATA from '@/constants/metadata';
 
 function MyApp({ Component, pageProps }: AppProps) {
   lockPageTitle();
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo
+        title={METADATA.TITLE}
+        description={METADATA.DESCRIPTION}
+        canonical={METADATA.HOST_URL}
+        openGraph={{
+          url: METADATA.HOST_URL,
+          title: METADATA.TITLE,
+          type: 'website',
+          description: METADATA.DESCRIPTION,
+          images: [
+            {
+              url: METADATA.IMAGE.URL,
+              width: METADATA.IMAGE.WIDTH,
+              height: METADATA.IMAGE.HEIGHT,
+              alt: METADATA.IMAGE.ALT,
+            },
+          ],
+          locale: METADATA.LOCALE,
+          site_name: METADATA.SITE_NAME,
+        }}
+        twitter={{
+          handle: 'session_app',
+          site: METADATA.HOST_URL,
+          cardType: 'summary_large_image',
+        }}
+        robotsProps={{
+          nosnippet: true,
+          notranslate: true,
+          noimageindex: true,
+          noarchive: true,
+          maxSnippet: -1,
+          maxImagePreview: 'large',
+          maxVideoPreview: -1,
+        }}
+        additionalMetaTags={[
+          {
+            property: 'viewport',
+            content:
+              'width=device-width, initial-scale=1.0, viewport-fit=cover',
+          },
+          {
+            property: 'apple-itunes-app',
+            content: 'app-id=1470168868',
+          },
+          {
+            property: 'msapplication-TileColor',
+            content: METADATA.MSAPPLICATION_TILECOLOR,
+          },
+          {
+            property: 'theme-color',
+            content: METADATA.THEME_COLOR,
+          },
+        ]}
+        additionalLinkTags={[
+          {
+            rel: 'icon',
+            type: 'type="image/png',
+            sizes: '32x32',
+            href: METADATA.FAVICON.MEDIUM,
+          },
+          {
+            rel: 'icon',
+            type: 'type="image/png',
+            sizes: '16x16',
+            href: METADATA.FAVICON.SMALL,
+          },
+          {
+            rel: 'apple-touch-icon',
+            sizes: '180x180',
+            href: METADATA.FAVICON.APPLE_TOUCH_ICON,
+          },
+          {
+            rel: 'manifest',
+            href: METADATA.MANIFEST,
+          },
+          {
+            rel: 'mask-icon',
+            href: METADATA.MASK_ICON.PATH,
+            color: METADATA.MASK_ICON.COLOR,
+          },
+          {
+            rel: 'shortlink',
+            href: METADATA.HOST_URL,
+          },
+          {
+            rel: 'alternative',
+            type: 'application/rss+xml',
+            href: '/feed',
+          },
+          {
+            rel: 'alternative',
+            type: 'application/atom+xml',
+            href: '/feed/atom',
+          },
+          {
+            rel: 'alternative',
+            type: 'application/feed+json',
+            href: '/feed/json',
+          },
+        ]}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
