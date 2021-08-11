737be75b049db38d2e4058c0c71daeab1fe1dd73
William Grant <willmgrant@gmail.com>
2021-08-11 11:31:50 +0200

    updated Features section

--- components/sections/Features/index.tsx
+++ components/sections/Features/index.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 import Image from 'next/image';
 import classNames from 'classnames';
 
+import Container from '@/components/Container';
 import { Headline } from '@/components/ui';
 
 export default function Features(): ReactElement {
@@ -13,17 +14,32 @@ export default function Features(): ReactElement {
     'md:mb-12'
   );
   return (
-    <section className={classNames(' text-gray-dark', 'lg:min-h-screen')}>
-      <div className={classNames('container p-6 mx-auto', 'md:p-12')}>
-        <Headline
-          color="gray-dark"
-          classes={classNames('text-lg font-semibold mt-8 mb-5', 'lg:mt-4')}
-        >
-          Features
-        </Headline>
+    <section className={classNames(' text-gray-dark')}>
+      <Headline
+        color="gray-dark"
+        classes={classNames('text-lg font-semibold pt-16', 'lg:pt-20')}
+        containerWidths={{
+          sm: '10rem',
+          md: '34rem',
+          lg: '67rem',
+        }}
+      >
+        Features
+      </Headline>
+      <Container
+        height={{
+          sm: '100%',
+          md: '100%',
+          lg: '100vh - 28px',
+        }}
+        classes={classNames(
+          '2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center'
+        )}
+      >
         <div
           className={classNames(
-            'lg:mx-auto lg:flex lg:justify-between lg:items-center'
+            'lg:mx-auto lg:flex lg:justify-between lg:items-center lg:pt-16',
+            '2xl:w-full'
           )}
         >
           <div
@@ -42,11 +58,7 @@ export default function Features(): ReactElement {
             />
           </div>
           <div
-            className={classNames(
-              'px-3',
-              'md:max-w-xl',
-              'lg:px-12 lg:max-w-lg lg:ml-4 lg:-mr-12'
-            )}
+            className={classNames('px-3', 'md:max-w-xl', 'lg:max-w-sm lg:px-0')}
           >
             <h3 className={headingClasses}>Group chats</h3>
             <p className={paragraphClasses}>
@@ -79,7 +91,7 @@ export default function Features(): ReactElement {
             />
           </div>
         </div>
-      </div>
+      </Container>
     </section>
   );
 }
