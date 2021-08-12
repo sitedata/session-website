66e0ad8fb569a1b197887f142fb5dd5520bdf6b1
William Grant <willmgrant@gmail.com>
2021-08-12 12:59:38 +0200

    added text scroll effect on mobile for about component

--- components/sections/About/index.tsx
+++ components/sections/About/index.tsx
@@ -1,17 +1,49 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState, useEffect, useRef } from 'react';
 import classNames from 'classnames';
 import redact from '@/utils/redact';
+import { useScreen } from '@/contexts/screen';
 
 import Container from '@/components/Container';
 import { Headline } from '@/components/ui';
 
 export default function About(): ReactElement {
-  const redactedClasses = redact({
+  const textRef = useRef<HTMLDivElement>(null);
+  const { isMobile, isTablet } = useScreen();
+  const redactedOptions = {
     redactColor: 'primary',
     textColor: 'white',
     animate: true,
     classes: 'p-1',
-  });
+  };
+  const [redactedClasses, setRedactedClasses] = useState(
+    redact(redactedOptions)
+  );
+  useEffect(() => {
+    if (isMobile || isTablet) {
+      const onScroll = () => {
+        const scrollEffectStart =
+          textRef.current?.offsetTop! - textRef.current?.scrollHeight! - 28;
+        const scrollEffectStop = textRef.current?.offsetTop! - 28;
+
+        if (
+          window.scrollY >= scrollEffectStart &&
+          window.scrollY < scrollEffectStop
+        ) {
+          setRedactedClasses(redact({ ...redactedOptions, disabled: true }));
+        }
+        if (
+          window.scrollY < scrollEffectStart ||
+          window.scrollY >= scrollEffectStop
+        ) {
+          setRedactedClasses(redact(redactedOptions));
+        }
+      };
+      document.addEventListener('scroll', onScroll);
+      return () => {
+        document.removeEventListener('scroll', onScroll);
+      };
+    }
+  }, [isMobile, isTablet]);
   return (
     <section className="text-white bg-gray-dark">
       <Headline
@@ -27,13 +59,13 @@ export default function About(): ReactElement {
       {/* Full screen height - Headline height */}
       <Container
         heights={{
-          sm: '100vh - 28px',
+          sm: '100%',
           md: '100vh + 2rem',
           lg: '100vh + 2rem',
         }}
         classes={classNames(
-          'flex flex-col justify-center items-center -mt-8',
-          'md:-mt-24',
+          'flex flex-col justify-center items-center pb-48',
+          'md:pb-0 md:-mt-24',
           'lg:-mt-32 lg:items-start'
         )}
       >
@@ -43,6 +75,7 @@ export default function About(): ReactElement {
             'md:text-4xl md:leading-relaxed md:ml-16',
             'lg:my-0 lg:ml-0 lg:max-w-2xl'
           )}
+          ref={textRef}
         >
           Session is an <span className={redactedClasses}>end-to-end</span>{' '}
           encrypted messenger that minimises{' '}
